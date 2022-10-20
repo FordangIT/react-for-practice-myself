d0cc34f3ad42fc3ac1ce7340bb34f45bb84d56df
FordangIT <fordang0819@gmail.com>
2022-10-21 00:24:45 +0900

    simple toDo list

---
+++ App.js
@@ -0,0 +1,33 @@
+import {useState, useEffect} from "react";
+function App(){
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event)=> setToDo(event. target.value)
+    const onSubmit = (event) => {
+        event.preventDefault()
+        if(toDo === ""){
+            return;
+        }
+        
+        setToDo((currentArray) => [toDo, ...currentArray])
+        setToDo("")
+    }; 
+    return(
+        <div>
+            <h1>My To Dos ({toDos.length})</h1>
+         <form onSubmit={onSubmit}>
+            <input 
+            value={toDo} 
+            onChange ={onChange} 
+            type="text" 
+            placeholder="wirte to do..."/>
+         </form>
+        <hr />
+        <ul>
+        {toDos.map((item, index) => {<li key={index}>{item}</li>}  )}
+        </ul>
+        </div>
+    )
+}
+
+export default App; 
